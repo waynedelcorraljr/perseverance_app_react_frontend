3e75d68dfea96744b6da6cb8af17097c2b854cb8
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-05-25 21:54:11 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <Provider >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
